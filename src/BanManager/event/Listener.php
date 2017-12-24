c9dc85113c03fc07d0cae2fc7cb52a8e81cd3c00
Taylcd <this@taylcd.com>
2017-12-24 10:26:47 +0800

    Processing ban message when the event not being cancelled

--- src/BanManager/event/Listener.php
+++ src/BanManager/event/Listener.php
@@ -17,21 +17,21 @@ class Listener implements \pocketmine\event\Listener{
     public function onPlayerLogin(PlayerLoginEvent $event){
         /** @var Ban $ban */
         if(($ban = $this->plugin->getDataProvider()->verifyPlayerLogin($event->getPlayer())) !== null && !$ban->isExpired()){
-            if($ban->getType() == Ban::BAN_TYPE_PLAYER){
-                $message = $this->plugin->getMessage("banned.player");
-            } else {
-                $message = $this->plugin->getMessage("banned.ipAddress");
-            }
-            if($ban->getExpireTime() === 0){
-                $message .= "\n" . $this->plugin->getMessage("banned.noExpire");
-            } else {
-                $message .= "\n" . $this->plugin->getMessage("banned.expireTime", date("Y/m/d", $ban->getExpireTime()), date("h:i", $ban->getExpireTime()));
-            }
-            if($ban->getReason() != null){
-                $message .= "\n" . $this->plugin->getMessage("banned.reason", $ban->getReason());
-            }
             $this->plugin->getServer()->getPluginManager()->callEvent($ev = new PlayerLoginFailedEvent($event->getPlayer(), $ban));
             if(!$ev->isCancelled()){
+                if($ban->getType() == Ban::BAN_TYPE_PLAYER){
+                    $message = $this->plugin->getMessage("banned.player");
+                } else {
+                    $message = $this->plugin->getMessage("banned.ipAddress");
+                }
+                if($ban->getExpireTime() === 0){
+                    $message .= "\n" . $this->plugin->getMessage("banned.noExpire");
+                } else {
+                    $message .= "\n" . $this->plugin->getMessage("banned.expireTime", date("Y/m/d", $ban->getExpireTime()), date("h:i", $ban->getExpireTime()));
+                }
+                if($ban->getReason() != null){
+                    $message .= "\n" . $this->plugin->getMessage("banned.reason", $ban->getReason());
+                }
                 $event->setKickMessage($message);
                 $event->setCancelled(true);
             }
